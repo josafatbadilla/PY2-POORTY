c18102e9a775ace0d3a0c88686c5c8263b813547
John Sanchez <jostsace@estudiantec.cr>
2021-11-17 18:38:51 -0600

    Juego Guess Who
    Inicializacion de los componentes graficos y carga de las imagenes para el guess who

--- POOrty/src/poorty/view/GuessWhoWindow.java
+++ POOrty/src/poorty/view/GuessWhoWindow.java
@@ -1,19 +1,170 @@
 
 package poorty.view;
 
+import java.awt.Font;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import poorty.controller.MainController;
+
 
 public class GuessWhoWindow extends javax.swing.JFrame {
-
+    
+    private ArrayList<JButton> gameOptions; // opciones para el juego
+    private ArrayList<ImageIcon> images; // las posibles imagenes para colocar en el panel
+    private String answer; // donde se guarda la respuesta correcta
+    private ImageIcon questionIcon;
+    private JLabel whoLabel;
+    private JLabel[][] coverMatrix;
+    private JLabel labelResult;
+    private JButton btnCloseGame;
+    
+    // establecimineto de valores para los componentes graficos
+    public final int LABEL_SIZE = 45; // tamano para el label con el question mark
+    public final int BUTTON_HEIGH = 30;
+    public final int MATRIX_SIZE = 10; // matriz sera de 10 x 10
+    
     public GuessWhoWindow() {
         initComponents();
+        // inicializacion de la matriz
+        this.coverMatrix = new JLabel[MATRIX_SIZE][MATRIX_SIZE];
+        this.images = new ArrayList<>();
+        this.gameOptions = new ArrayList<>();
+        initGame();
     }
 
+    
+    // inicializacion de todos los componentes necesarios para el juego
+        // colocacion de la imgagen aleatoria
+        //Carga de botones
+        // guardar el nombre de la opcion correcta
+        // creacion de la matriz de labels con el signo de pregunta
+        // de forma aleatoria quita algunos
+    private void initGame(){
+        // se cargan las imagenes del folder guess who
+        File folder = new File("./src/media/guessWho");
+        ImageIcon cardIcon;
+        int btnPos = 0;
+        for(File file: folder.listFiles()){
+            if(!file.isDirectory()){
+                try{
+                    // que no sea el icon de cuando esta oculto
+                    if(!file.getName().replaceFirst("[.][^.]+$", "").equals("Question")){
+                        // se crean las posibles imagenes a poner de fondo
+                        // su descripcion es el nombre del archivo
+                        images.add(new ImageIcon(file.getCanonicalPath(), file.getName().replaceFirst("[.][^.]+$", ""))); 
+                        JButton btnOp = createOptionButton(file.getName().replaceFirst("[.][^.]+$", ""), btnPos);
+                        gameOptions.add(btnOp); // se agrea a la lista de opciones
+                        pnlOptions.add(btnOp); // se agrega al panel de opciones
+                        btnPos++;
+                    }else{
+                        this.questionIcon = new ImageIcon(file.getCanonicalPath()); // imagen del question mark
+                    }
+                   
+                    // el replaceFirst elimina la extension del archivo para obtener simplemente su nombre base
+                }catch(IOException e){
+                    System.out.println("Error al cargar personaje");
+                }
+ 
+            }
+        }
+        
+        // seteo del label de finalizacion y el boton de terminar el juego
+        setFinalOptionSection(btnPos);
+        
+        generateCoverMatrix();
+        setGuessWho();
+    }
+   
+    
+    // escoge una imgaen de forma aleatoria y se la coloca al panel
+    private void setGuessWho(){
+        int randIndex = new Random().nextInt(images.size());
+        this.whoLabel = new JLabel(MainController.resizeIcon(images.get(randIndex), pnlWho.getWidth(), pnlWho.getHeight()));
+        this.whoLabel.setBounds(0,0, pnlWho.getWidth(), pnlWho.getHeight());
+        this.pnlWho.add(whoLabel);
+        
+        this.answer = images.get(randIndex).getDescription(); // obtiene la descripcion
+
+    }
+    
+    private JButton createOptionButton(String text, int btnPos){
+        JButton newButton = new JButton(text);
+        newButton.setBounds(0, btnPos * BUTTON_HEIGH, pnlOptions.getWidth(), BUTTON_HEIGH);
+        newButton.setFont(new Font("Verdana", Font.PLAIN, 12));
+        newButton.setHorizontalAlignment(SwingConstants.CENTER);
+        return newButton;
+    }
+    
+    private void setFinalOptionSection(int lastBtnPos){
+        this.labelResult = new JLabel("");
+        this.labelResult.setBounds(0, lastBtnPos * BUTTON_HEIGH, pnlOptions.getWidth(), BUTTON_HEIGH);
+        this.labelResult.setFont(new Font("Verdana", Font.PLAIN, 12));
+        this.labelResult.setHorizontalAlignment(SwingConstants.CENTER);
+        pnlOptions.add(labelResult);
+        this.btnCloseGame = new JButton("Salir");
+        this.btnCloseGame.setEnabled(false);
+        this.btnCloseGame.setBounds(0, (lastBtnPos + 1) * BUTTON_HEIGH, pnlOptions.getWidth(), BUTTON_HEIGH);
+        this.btnCloseGame.setFont(new Font("Verdana", Font.PLAIN, 12));
+        this.btnCloseGame.setHorizontalAlignment(SwingConstants.CENTER);
+        pnlOptions.add(btnCloseGame);
+    }
+
+    // generacion de la matriz para ocultar la imagen
+    private void generateCoverMatrix(){
+        int x = 5, y = 5;
+        for (int i = 0; i < MATRIX_SIZE; i++) {
+            for (int j = 0; j < MATRIX_SIZE; j++) {
+                // creacion de los labels de covertura
+                JLabel newLabel = createCoverLabel(x, y);
+                this.coverMatrix[i][j] = newLabel;
+                this.pnlWho.add(newLabel);
+                
+                x += LABEL_SIZE + 5; // + 5 para el margen
+            }
+            x = 5;
+            y += LABEL_SIZE + 5;
+        }
+        
+        // mostrar de forma aleatoria partes de la imagen
+        unCoverLabels();
+    }
+    
+    private JLabel createCoverLabel(int x, int y){
+        JLabel newLabel = new JLabel(MainController.resizeIcon(questionIcon, LABEL_SIZE, LABEL_SIZE));
+        newLabel.setBounds(x, y, LABEL_SIZE, LABEL_SIZE);
+        return newLabel;
+    }
+    
+    private void unCoverLabels(){
+        // genera un numero entre 4 y 8
+        int uncoverLabels = new Random().nextInt(8-4+1) + 4;
+        
+        for(int i = 0; i < uncoverLabels; i++){
+            boolean uncover = false;
+            while(!uncover){
+                int iMatrix = new Random().nextInt(MATRIX_SIZE);
+                int jMatrix = new Random().nextInt(MATRIX_SIZE);
+                
+                if(coverMatrix[iMatrix][jMatrix].isVisible()){
+                    coverMatrix[iMatrix][jMatrix].setVisible(false);
+                    uncover = true;
+                }
+            }
+        
+        }
+    }
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         pnlWho = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
         pnlOptions = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
 
@@ -22,24 +173,15 @@ public class GuessWhoWindow extends javax.swing.JFrame {
         pnlWho.setBackground(new java.awt.Color(255, 255, 255));
         pnlWho.setPreferredSize(new java.awt.Dimension(500, 500));
 
-        jLabel2.setBackground(new java.awt.Color(255, 51, 51));
-        jLabel2.setOpaque(true);
-
         javax.swing.GroupLayout pnlWhoLayout = new javax.swing.GroupLayout(pnlWho);
         pnlWho.setLayout(pnlWhoLayout);
         pnlWhoLayout.setHorizontalGroup(
             pnlWhoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(pnlWhoLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(445, Short.MAX_VALUE))
+            .addGap(0, 508, Short.MAX_VALUE)
         );
         pnlWhoLayout.setVerticalGroup(
             pnlWhoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(pnlWhoLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(444, Short.MAX_VALUE))
+            .addGap(0, 506, Short.MAX_VALUE)
         );
 
         pnlOptions.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
@@ -48,11 +190,11 @@ public class GuessWhoWindow extends javax.swing.JFrame {
         pnlOptions.setLayout(pnlOptionsLayout);
         pnlOptionsLayout.setHorizontalGroup(
             pnlOptionsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 186, Short.MAX_VALUE)
+            .addGap(0, 190, Short.MAX_VALUE)
         );
         pnlOptionsLayout.setVerticalGroup(
             pnlOptionsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGap(0, 504, Short.MAX_VALUE)
         );
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
@@ -68,9 +210,9 @@ public class GuessWhoWindow extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(pnlWho, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(pnlWho, javax.swing.GroupLayout.PREFERRED_SIZE, 508, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(pnlOptions, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                        .addComponent(pnlOptions, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -79,53 +221,49 @@ public class GuessWhoWindow extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(pnlOptions, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGap(21, 21, 21))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(pnlWho, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                    .addComponent(pnlOptions, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(pnlWho, javax.swing.GroupLayout.DEFAULT_SIZE, 506, Short.MAX_VALUE))
+                .addGap(21, 21, 21))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
 
-//    public static void main(String args[]) {
-//        /* Set the Nimbus look and feel */
-//        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-//        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-//         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-//         */
-//        try {
-//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-//                if ("Nimbus".equals(info.getName())) {
-//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-//                    break;
-//                }
-//            }
-//        } catch (ClassNotFoundException ex) {
-//            java.util.logging.Logger.getLogger(GuessWhoWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (InstantiationException ex) {
-//            java.util.logging.Logger.getLogger(GuessWhoWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (IllegalAccessException ex) {
-//            java.util.logging.Logger.getLogger(GuessWhoWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-//            java.util.logging.Logger.getLogger(GuessWhoWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-//        }
-//        //</editor-fold>
-//
-//        /* Create and display the form */
-//        java.awt.EventQueue.invokeLater(new Runnable() {
-//            public void run() {
-//                new GuessWhoWindow().setVisible(true);
-//            }
-//        });
-//    }
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(GuessWhoWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(GuessWhoWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(GuessWhoWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(GuessWhoWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new GuessWhoWindow().setVisible(true);
+            }
+        });
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel pnlOptions;
     private javax.swing.JPanel pnlWho;
     // End of variables declaration//GEN-END:variables
